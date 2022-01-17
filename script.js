1e8112054c1b2ae50840ae4f6a822158708f7472
paulakozinecaa <p.kozineca@gmail.com>
2022-01-17 15:04:00 +0200

    sakum

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert(1)
+const vardi=['Artūrs Liepa', 'Daniels Ozols', 'Ieva Kļava', 'Valdis Roze', 'Katrīna Telts'];
+const balvas=['mašīna','skrīveru saldumi', 'telefons', 'kvadracikls', 'ceļojums'];
+const naudaKopa=1000000;
+let uzvaretajuSkaits=5;
+let rand=Math.random()*10;
+rand=Math.floor(rand); //noapalo skaitli uz leju
+console.log(vardi[rand]); //izvada konsolee random vardus
