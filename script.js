3033d43821c585dc68f9972c5a7e3484366a46f3
paulakozinecaa <p.kozineca@gmail.com>
2022-01-24 15:03:56 +0200

    Otrā stunda

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const vardi=['Artūrs Liepa', 'Daniels Ozols', 'Ieva Kļava', 'Valdis Roze', 'Ka
 const balvas=['mašīna','skrīveru saldumi', 'telefons', 'kvadracikls', 'ceļojums'];
 const naudaKopa=1000000;
 let uzvaretajuSkaits=5;
-let rand=Math.random()*5;
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for(let i=0;i<5;i++){ //jasak skaitit ar 0. jo tad ari 5 aizies
+let rand=Math.random()*vardi.length;
 rand=Math.floor(rand); //noapalo skaitli uz leju
 console.log(vardi[rand]); //izvada konsolee random vardus
+}
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
